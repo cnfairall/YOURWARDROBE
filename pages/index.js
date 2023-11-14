f5c810452565a0fec145139d8aee7dcbe6e4ae86
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-11-14 15:37:49 -0600

    separate carousel version

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ItemCarousels from '../components/Carousels';
+import { BottomCarousel, TopCarousel } from '../components/Carousels';
 
 export default function Generate() {
   return (
     <>
-      <ItemCarousels />
+      <TopCarousel />
+      <BottomCarousel />
     </>
   );
 }
